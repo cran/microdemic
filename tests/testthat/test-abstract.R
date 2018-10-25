02a3428126101ec3f76afad22b6cd4702ac39123
Scott Chamberlain <myrmecocystus+r@gmail.com>
2018-10-25 03:30:21 +0000

    version 0.4.0

--- tests/testthat/test-abstract.R
+++ tests/testthat/test-abstract.R
@@ -1,11 +1,14 @@
 context("ma_abstract")
 
-test_that("ma_evaluate works as expected", {
+test_that("ma_abstract works as expected", {
   skip_on_cran()
-  Sys.sleep(1)
 
-  aa <- ma_abstract(query = "Y='19'...", count = 10)
+  vcr::use_cassette("ma_abstract", {
+    aa <- ma_abstract(query = "Y='19'...", count = 10)
 
-  expect_is(aa, "character")
-  expect_equal(length(aa), 10)
+    expect_is(aa, "tbl_df")
+    expect_is(aa$Id, "numeric")
+    expect_is(aa$abstract, "character")
+    expect_equal(NROW(aa), 10)
+  }, preserve_exact_body_bytes = TRUE)
 })
--- tests/testthat/test-similarity.R
+++ tests/testthat/test-similarity.R
@@ -2,7 +2,6 @@ context("ma_similarity")
 
 test_that("ma_similarity works as expected", {
   skip_on_cran()
-  Sys.sleep(1)
 
   s1 <- "Using complementary priors, we derive a fast greedy algorithm that
 	can learn deep directed belief networks one layer at a time, provided the
@@ -12,7 +11,8 @@ test_that("ma_similarity works as expected", {
 	machine learning systems. However, overfitting is a serious problem in
 	such networks"
 
-	aa <- ma_similarity(s1, s2)
-
-  expect_type(aa, "double")
+  vcr::use_cassette("ma_similarity", {
+  	aa <- ma_similarity(s1, s2)
+    expect_type(aa, "double")
+  }, preserve_exact_body_bytes = TRUE)
 })
