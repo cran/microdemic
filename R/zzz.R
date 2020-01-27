9fe0a9a5bc81e07f9cf3b7e1dba60fca2ec74697
Scott Chamberlain <myrmecocystus+r@gmail.com>
2020-01-27 21:00:02 +0000

    version 0.5.0

--- R/zzz.R
+++ R/zzz.R
@@ -17,7 +17,7 @@ ma_HTTP <- function(path, args, key, method = "GET", body = list(),
     GET = cli$get(path, query = args, ...),
     POST = cli$post(path, query = args, body = body, encode = encode, ...)
   )
-  res$raise_for_status()
+  raise_for_status2(res)
   txt <- res$parse("UTF-8")
   jsonlite::fromJSON(txt, flatten = TRUE)
 }
@@ -48,4 +48,52 @@ assert <- function(x, y) {
            paste0(y, collapse = ", "), call. = FALSE)
     }
   }
+}
+
+raise_for_status2 <- function(res) {
+  if (res$status_code >= 300) {
+    er_lst <- tryCatch(
+      parse_error_msg(res),
+      error = function(e) httpcode::http_code(res$status_code)
+    )
+    er_msg <- sprintf(
+      "%s (HTTP %s)\nExplanation: %s",
+      res$status_code, er_lst$message, er_lst$explanation
+    )
+    stop(er_msg, call. = FALSE)
+  }
+}
+
+parse_error_msg <- function(res) {
+  txt <- res$parse("UTF-8")
+  er_vec <- jsonlite::fromJSON(txt)
+  er_vec <- all_to_lower(er_vec)
+  # er_vec names used to be capitilized (e.g., "Error.Code", not "error.code").
+  # convert to lowercase just in case API goes back to using caps.
+  # names(er_vec) <- tolower(names(er_vec))
+  tmp <- list(
+    # the error.code element refers to a short error message, not the HTTP
+    # status code
+    message = er_vec$error$code,
+    explanation = er_vec$error$message
+  )
+  if (!is.null(er_vec$error$innererrors)) {
+    tmp$explanation <- er_vec$error$innererrors$message[1]
+  }
+  return(tmp)
+}
+
+# x = list(A = list(B = 6), C = list(D = 7, E = list(F = list(G = 7))))
+# all_to_lower(x)
+all_to_lower <- function(x) {
+  if (!is.null(names(x))) {
+    names(x) <- tolower(names(x))
+  }
+  if (is.list(x)) {
+    x <- lapply(x, all_to_lower)
+  }
+  if (is.data.frame(x)) {
+    names(x) <- tolower(names(x))
+  }
+  x
 }
